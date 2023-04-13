88bb8ada17768f42024c856e9a1046537f7d0536
Prashil <Prashiln79@gmail.com>
2023-04-13 13:17:50 +0530

    sound added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Howl, Howler } from 'howler';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'noise';
+  public soundList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  sound = new Howl({
+    src: ['assets/music/rain.mp3']
+  });
+  private volume = 0;
+  private playSound = false;
+
+  constructor() {
+
+  }
+
+  play() {
+    this.playSound = !this.playSound;
+    this.playSound ? this.sound.play() : this.sound.stop();
+  
+  }
+
+
+  increaseVolume(){
+    this.sound.volume(this.volume=this.volume+ 0.1);
+  }
+
+  decreaseVolume(){
+    this.sound.volume(this.volume=this.volume-0.1);
+  }
 }
