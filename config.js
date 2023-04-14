0c443155520399a5add0005f54956cf704d33c56
Prashil <Prashiln79@gmail.com>
2023-04-14 14:16:33 +0530

    ENV added

--- config.js
+++ config.js
@@ -1,26 +1,30 @@
 const setEnv = () => {
   const fs = require('fs');
   const writeFile = fs.writeFile;
+  const dotenv = require('dotenv');
+  dotenv.config();
   // Configure Angular `environment.ts` file path
   const targetPath = './src/environments/environment.ts';
   // Load node modules
- 
+
   // `environment.ts` file structure
   const envConfigFile = `export const environment = {
 
     production: false,
     firebase: {
-      apiKey: '${process.GOOGLE_API_KEY}',
-      authDomain: '${process.AUTH_DOMAIN}',
-      databaseURL: '${process.DATA_BASE_URL}',
-      projectId: '${process.PROJECT_ID}',
-      storageBucket: '${process.STORAGE_BUCKET}',
-      messagingSenderId:'${process.MESSAGING_SENDER_ID}',
-      appId: '${process.APP_ID}',
-      measurementId: '${process.MEASUREMENT_ID}',
+      apiKey: '${process.env.GOOGLE_API_KEY}',
+      authDomain: '${process.env.AUTH_DOMAIN}',
+      databaseURL: '${process.env.DATA_BASE_URL}',
+      projectId: '${process.env.PROJECT_ID}',
+      storageBucket: '${process.env.STORAGE_BUCKET}',
+      messagingSenderId:'${process.env.MESSAGING_SENDER_ID}',
+      appId: '${process.env.APP_ID}',
+      measurementId: '${process.env.MEASUREMENT_ID}',
     }
 };
 `;
+
+  console.log(envConfigFile);
   writeFile(targetPath, envConfigFile, (err) => {
     if (err) {
       console.error(err);
