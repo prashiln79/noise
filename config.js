8cb3b92c70269223ce47017bc851da8f83acceaa
Prashil <Prashiln79@gmail.com>
2023-04-14 13:21:05 +0530

    Env changes

--- config.js
+++ config.js
@@ -4,11 +4,7 @@ const setEnv = () => {
   // Configure Angular `environment.ts` file path
   const targetPath = './src/environments/environment.ts';
   // Load node modules
-  const colors = require('colors');
-  const appVersion = require('../../package.json').version;
-  require('dotenv').config({
-    path: 'src/environments/.env'
-  });
+ 
   // `environment.ts` file structure
   const envConfigFile = `export const environment = {
 
@@ -25,13 +21,12 @@ const setEnv = () => {
     }
 };
 `;
-  console.log(colors.magenta('The file `environment.ts` will be written with the following content: \n'));
   writeFile(targetPath, envConfigFile, (err) => {
     if (err) {
       console.error(err);
       throw err;
     } else {
-      console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
+      console.log(`Angular environment.ts file generated correctly at ${targetPath} \n`);
     }
   });
 };
