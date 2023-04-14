b52fffb597335f4cc12e708cd00876851dbc7ce6
Prashil <Prashiln79@gmail.com>
2023-04-14 16:58:23 +0530

    sadsad

--- config.js
+++ config.js
@@ -13,13 +13,13 @@ const setEnv = () => {
 
     production: false,
     firebase: {
-      apiKey: '${env.GOOGLE_API_KEY}',
+      apiKey: '${secrets.TOKEN}',
       authDomain: '${process.env.AUTH_DOMAIN}',
       databaseURL: '${process.env.DATA_BASE_URL}',
       projectId: '${process.env.PROJECT_ID}',
       storageBucket: '${process.env.STORAGE_BUCKET}',
       messagingSenderId:'${process.env.MESSAGING_SENDER_ID}',
-      appId: '${env.APP_ID}',
+      appId: '${process.env.APP_ID}',
       measurementId: '${process.env.MEASUREMENT_ID}',
     }
 };
