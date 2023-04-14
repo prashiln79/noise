81e29de8f13c7d2074234a1374ddad4d93f011b9
Prashil <Prashiln79@gmail.com>
2023-04-14 17:02:12 +0530

    ss

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const setEnv = () => {
 
     production: false,
     firebase: {
-      apiKey: '${process.env.secrets.TOKEN || process.env.TOKEN}',
+      apiKey: '${process.env.TOKEN}',
       authDomain: '${process.env.AUTH_DOMAIN}',
       databaseURL: '${process.env.DATA_BASE_URL}',
       projectId: '${process.env.PROJECT_ID}',
