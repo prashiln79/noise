85d98da3e18ba946f51466c02b98c30d0c53256c
Prashil <Prashiln79@gmail.com>
2023-04-14 15:28:34 +0530

    asasda

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,14 @@
-
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "AIzaSyCTJJ4_XJ9IgWDAa0KalKUGlYFVam0fQC8",
-    authDomain: "noise-48aa2.firebaseapp.com",
-    databaseURL: "https://noise-48aa2-default-rtdb.firebaseio.com/",
-    projectId: "noise-48aa2",
-    storageBucket: "noise-48aa2.appspot.com",
-    messagingSenderId: "776621040347",
-    appId: "1:776621040347:web:5197024b1bc9e2da8d62fe",
-    measurementId: "G-0SC8H9XQCC"
-  }
-};
 
+    production: false,
+    firebase: {
+      apiKey: 'AIzaSyCTJJ4_XJ9IgWDAa0KalKUGlYFVam0fQC8',
+      authDomain: 'noise-48aa2.firebaseapp.com',
+      databaseURL: 'https://noise-48aa2-default-rtdb.firebaseio.com/',
+      projectId: 'noise-48aa2',
+      storageBucket: 'noise-48aa2.appspot.com',
+      messagingSenderId:'776621040347',
+      appId: '1:776621040347:web:5197024b1bc9e2da8d62fe',
+      measurementId: 'G-0SC8H9XQCC',
+    }
+};
