64b80ca587c9e1f0d25e82983ad7b91cf29c5081
Prashil <Prashiln79@gmail.com>
2023-04-14 17:16:59 +0530

    sdfd

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const setEnv = () => {
 
     production: false,
     firebase: {
-      apiKey: '${process.env.TOKEN}',
+      apiKey: '${process.env.GOOGLE_API_KEY}',
       authDomain: '${process.env.AUTH_DOMAIN}',
       databaseURL: '${process.env.DATA_BASE_URL}',
       projectId: '${process.env.PROJECT_ID}',
